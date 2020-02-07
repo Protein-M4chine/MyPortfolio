5760bd4a2054aa1d3709d39e73f5c6f13500f5b3
Sean Robinson <robinsons1@etsu.edu>
2020-02-07 14:21:06 -0600

    First Commit

---
+++ robinsonwebdev.com/wwwroot/js/canvas.js
@@ -0,0 +1,47 @@
+﻿////chart script
+
+//let skillsChart = document.getElementById('skillsChart').getContext('2d');
+
+//let barChart = new Chart(skillsChart, {
+//    type: 'bar',
+//    data: {
+//        labels: ['HTML/CSS', 'PHP', 'C#', 'Javascript/jQuery/AJAX', 'Python', 'SQL', 'LINQ', 'ASP.NET', 'XAMP', 'Azure', 'AWS'],
+//        datasets: [{
+//            label: 'Web Tech Skills',
+//            data: [
+//                100,
+//                50,
+//                60,
+//                40,
+//                20,
+//                50,
+//                20,
+//                50,
+//                20,
+//                30,
+//                30
+//            ],
+//            backgroundColor: [
+//                'rgba(255, 99, 132, 0.7)',
+//                'rgba(54, 162, 235, 0.7)',
+//                'rgba(255, 206, 86, 0.7)',
+//                'rgba(66, 296, 124, 0.7)',
+//                'rgba(255, 159, 64, 0.7)',
+//                'rgba(30, 99, 255, 0.7)',
+//                'rgba(255, 20, 10, 0.7)',
+//                'rgba(255, 130, 200, 0.7)',
+//                'rgba(255, 206, 255, 0.7)',
+//                'rgba(255, 102, 3, 0.7)',
+//                'rgba(75, 192, 192, 0.7)'
+//            ]
+//        }]
+
+//    },
+//    options: {
+//        responsive: true,
+//        title: {
+//            display: true,
+//            text: 'skills'
+//        }
+//    }
+//});
---
+++ robinsonwebdev.com/wwwroot/js/site.js
@@ -0,0 +1,81 @@
+﻿//global variables
+
+//global functions
+
+function showHomePage() {
+    $("#home").show(750);
+    $("#about").hide();
+    $("#projects").hide();
+    $("#contact").hide();
+
+    localStorage.setItem("pageHolder", "home");
+}
+
+function showAboutPage() {
+    $("#about").show(750);
+    $("#home").hide();
+    $("#projects").hide();
+    $("#contact").hide();
+
+    localStorage.setItem("pageHolder", "about");
+}
+
+function showProjectsPage() {
+    $("#projects").show(750);
+    $("#home").hide();
+    $("#about").hide();
+    $("#contact").hide();
+
+    localStorage.setItem("pageHolder", "projects");
+}
+
+function showContactPage() {
+    $("#contact").show(750);
+    $("#home").hide();
+    $("#about").hide();
+    $("#projects").hide();
+
+    localStorage.setItem("pageHolder", "contact");
+}
+
+
+
+
+$(document).ready(function () {
+    if (performance.navigation.type == 1) {
+        console.info("Page was reloaded");
+        console.info(localStorage.pageHolder);
+        if (localStorage.pageHolder == "home") {
+            showHomePage();
+        }
+        else if (localStorage.pageHolder == "about") {
+            showAboutPage();
+        }
+        else if (localStorage.pageHolder == "projects") {
+            showProjectsPage();
+        }
+        else if (localStorage.pageHolder == "contact") {
+            showContactPage();
+        }
+
+    }
+    else
+        console.info("Page was normally loaded");
+
+    $(".homelink").click(function () {
+        showHomePage();
+    });
+
+    $(".aboutlink").click(function () {
+        showAboutPage();
+    });
+
+    $(".projectslink").click(function () {
+        showProjectsPage();
+    });
+
+    $(".contactlink").click(function () {
+        showContactPage();
+    });
+});
+
